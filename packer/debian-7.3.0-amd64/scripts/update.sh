fde4c9fa4b54fe83df177c5df8aad7f9ea5112c5
Achim Rosenhagen <a.rosenhagen@ffuenf.de>
2014-04-03 12:44:59 +0200

    install curl to fix broken wget while retrieving content from secured sites

--- packer/debian-7.3.0-amd64/scripts/update.sh
+++ packer/debian-7.3.0-amd64/scripts/update.sh
@@ -2,3 +2,6 @@
 
 apt-get update
 apt-get -y upgrade
+
+# install curl to fix broken wget while retrieving content from secured sites
+apt-get -y install curl
--- packer/ubuntu-12.04.4-server-amd64/scripts/update.sh
+++ packer/ubuntu-12.04.4-server-amd64/scripts/update.sh
@@ -6,6 +6,9 @@ apt-get -y upgrade
 # ensure the correct kernel headers are installed
 apt-get -y install linux-headers-$(uname -r)
 
+# install curl to fix broken wget while retrieving content from secured sites
+apt-get -y install curl
+
 # update package index on boot
 cat <<EOF > /etc/init/refresh-apt.conf
 description "update package index"
