7894d0278bfdfab92135743db6e706b8b0e61172
Ties de Kock <ties@tiesdekock.nl>
2016-02-14 23:07:38 +0100

    Use wily kernel

--- packer/scripts/release_specific/trusty_lts_kernel.sh
+++ packer/scripts/release_specific/trusty_lts_kernel.sh
@@ -1,2 +1,2 @@
 # ensure the utopic kernel is installed. Current kernel and headers will be deleted during cleanup.
-apt-get install -y linux-generic-lts-utopic linux-headers-generic-lts-utopic
+apt-get install -qy linux-generic-lts-wily linux-headers-generic-lts-wily
