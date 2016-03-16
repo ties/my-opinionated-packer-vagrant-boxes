79b86b7892596209c66193101757b43eec7afa7f
Ties de Kock <ties@tiesdekock.nl>
2016-03-16 11:44:25 +0100

    Attempt virtio-scsi

--- build_boxes.sh
+++ build_boxes.sh
@@ -3,23 +3,23 @@
 bundle exec thor packer:build \
   --atlas_version=1.0.23 \
   --os=debian \
-  --os_version=7.9.0 \
-  --providers=virtualbox,vmware,parallels
+  --os_version=8.3.0 \
+  --providers=qemu
 
 bundle exec thor packer:build \
   --atlas_version=1.0.23 \
   --os=debian \
-  --os_version=8.3.0 \
-  --providers=virtualbox,vmware,parallels
+  --os_version=8.3.0-puppet4 \
+  --providers=qemu
+
+bundle exec thor packer:build \
+  --atlas_version=1.0.23 \
+  --os=ubuntu \
+  --os_version=14.04.4-puppet4-server \
+  --providers=qemu
 
 bundle exec thor packer:build \
   --atlas_version=1.0.23 \
   --os=ubuntu \
   --os_version=14.04.4-server \
-  --providers=virtualbox,vmware,parallels
-
-bundle exec thor packer:build \
-  --atlas_version=1.0.23 \
-  --os=ubuntu \
-  --os_version=15.10-server \
-  --providers=virtualbox,parallels
+  --providers=qemu
--- packer/scripts/filesystem.sh
+++ packer/scripts/filesystem.sh
@@ -5,9 +5,12 @@
 # - /tmp on tmpfs
 #
 sed --in-place -e 's/\(.*swap.*sw\)/\1,discard/g' /etc/fstab
-sed --in-place -e 's/\(defaults\)/\1,acl,discard/g' /etc/fstab
-mount -o remount /
+sed --in-place -e 's/\(ext4[[:space:]]\+\)/\1acl,discard,/g' /etc/fstab
+mount -o remount,discard /
 
-echo "tmpfs /tmp tmpfs defaults,noatime,nodev,mode=1777,size=1024M 0 0" >> /etc/fstab
+cat > /etc/fstab << EOF
+tmpfs /tmp tmpfs defaults,noatime,nodev,mode=1777,size=1024M 0 0
+EOF
+
 rm -rf /tmp/*
-mount -o remount /tmp
+mount /tmp
